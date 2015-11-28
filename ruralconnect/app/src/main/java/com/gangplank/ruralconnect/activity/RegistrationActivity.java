4a77179298f1d0c68935eeb88255e8e1f34539cd
Aruna <arunash@thoughtworks.com>
2015-11-28 18:11:42 +0530

    Pushing registration screen

--- ruralconnect/app/src/main/java/com/gangplank/ruralconnect/activity/RegistrationActivity.java
+++ ruralconnect/app/src/main/java/com/gangplank/ruralconnect/activity/RegistrationActivity.java
@@ -1,27 +1,20 @@
 package com.gangplank.ruralconnect.activity;
 
-import android.content.Intent;
 import android.app.Activity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import com.gangplank.ruralconnect.R;
 
-
-public class LoginActivity extends Activity{
+public class RegistrationActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
 
-    }
+        setContentView(R.layout.activity_registration);
 
-    public void register(View view){
-        Intent myIntent = new Intent(getApplicationContext(),RegistrationActivity.class);
-        startActivity(myIntent);
     }
 
     public void login(View view) {
@@ -29,4 +22,3 @@ public class LoginActivity extends Activity{
         startActivity(myIntent);
     }
 }
-
